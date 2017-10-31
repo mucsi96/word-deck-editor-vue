2fff06c0970434adbf2948d4a3f570a56c5f1dca
Igor Muchychka <mucsi_96@yahoo.com>
2017-10-31 05:28:09 +0100

    Preloading started

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -5,6 +5,7 @@
       v-for="word in deck"
       :key="word.front"
       :to="{ name: 'word', params: { id: getWordId(word) }}"
+      :class="{ disabled: !word.preloaded }"
     >
       <div class="header">{{word.front}}</div>
       {{word.back}}
@@ -26,7 +27,19 @@ export default {
       return this.$store.state.deck;
     },
   },
+  created() {
+    setTimeout(() => this.preload(), 100);
+  },
   methods: {
+    async preload() {
+      const notPreloadedWord = this.deck.find(word => !word.preloaded);
+      if (notPreloadedWord) {
+        console.log(`Preloading ${notPreloadedWord.front}...`);
+        await this.$http.get(`meta/${encodeURIComponent(notPreloadedWord.frontLanguage)}/${encodeURIComponent(notPreloadedWord.front.toLowerCase())}`);
+        this.$store.commit('updateWord', { word: notPreloadedWord.front, prop: 'preloaded', value: true });
+      }
+      setTimeout(() => this.preload(), 100);
+    },
     getWordId(word) {
       return encodeURIComponent(word.front);
     },
