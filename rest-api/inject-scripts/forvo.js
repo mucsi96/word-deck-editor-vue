ef970ba7216e8de08b8bb70f52b784bb47966765
Igor Muchychka <mucsi_96@yahoo.com>
2017-11-05 18:41:46 +0100

    Fix forvo integration

--- rest-api/inject-scripts/forvo.js
+++ rest-api/inject-scripts/forvo.js
@@ -1,29 +1,31 @@
 /* global _AUDIO_HTTP_HOST, base64_decode */
 
-const getWordPronunciations = () => Array.prototype.map.call(document.querySelectorAll('.list-words>ul>li'), (el) => {
-  const word = el.querySelector('.word').textContent;
+const getWordPronunciations = () => Array.prototype.map.call(document.querySelectorAll('.list-words>ul>li .word'), (el) => {
+  const wordEl = el.querySelector('.word');
+  if (!wordEl) return null;
   const playScript = el.querySelector('.play').getAttribute('onClick');
   const base64 = playScript.match(/^Play\((\d+),'([a-zA-Z0-9=]+)'/)[2];
   const sound = `https://${_AUDIO_HTTP_HOST}/mp3/${base64_decode(base64)}`;
 
   return {
-    word,
+    word: wordEl.textContent,
     sound,
   };
 });
 
 const getPhrasePronunciations = () => Array.prototype.map.call(document.querySelectorAll('.list-phrases>ul>li'), (el) => {
-  const word = el.querySelector('.word').textContent;
+  const wordEl = el.querySelector('.word');
+  if (!wordEl) return null;
   const playScript = el.querySelector('.play').getAttribute('onClick');
   const base64 = playScript.match(/^PlayPhrase\((\d+),'([a-zA-Z0-9=]+)'/)[2];
   const sound = `https://${_AUDIO_HTTP_HOST}/phrases/mp3/${base64_decode(base64)}`;
 
   return {
-    word,
+    word: wordEl.textContent,
     sound,
   };
 });
 
 return {
-  pronunciations: [...getWordPronunciations(), ...getPhrasePronunciations()],
+  pronunciations: [...getWordPronunciations(), ...getPhrasePronunciations()].filter(item => item),
 };
